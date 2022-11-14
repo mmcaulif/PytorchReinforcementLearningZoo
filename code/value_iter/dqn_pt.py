235ea2b1cb952f225bd620a5624b9d74345a62ee
mmcaulif <mmcaulif@tcd.ie>
2022-11-14 17:40:55 +0000

    standard dqn implementation

--- code/value_iter/dqn_pt.py
+++ code/value_iter/dqn_pt.py
@@ -1,18 +1,21 @@
-from collections import deque
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.utils import clip_grad_norm_
-import numpy as np
 import gym
 import copy
+from collections import deque
 from gym.wrappers import RecordEpisodeStatistics
 
-from code.utils.models import Q_val, Q_duelling
-from code.utils.memory import ReplayBuffer
+from code.utils.models import Q_val
+# from code.utils.memory import 
 
+"""
+1. Use a replay buffer class 
+2. copy and paste and begin work on C51
+3. Keep this as vanilla DQN
+"""
 
-class DDQN:
+class DQN():    
     def __init__(
         self,
         environment,
@@ -46,17 +49,12 @@ class DDQN:
         self.verbose = verbose
 
     def update(self, batch):
-        s = torch.from_numpy(np.array(batch.s)).type(torch.float32)
-        a = torch.from_numpy(np.array(batch.a)).unsqueeze(1).type(torch.float32)
-        r = torch.FloatTensor(batch.r).unsqueeze(1)
-        s_p = torch.from_numpy(np.array(batch.s_p)).type(torch.float32)
-        d = torch.IntTensor(batch.d).unsqueeze(1)
+        s, a, r, s_p, d = batch
 
         q = self.q_func(s).gather(1, a.long())
 
         with torch.no_grad():
-            a_p = torch.argmax(self.q_func(s_p), dim = 1).unsqueeze(1)  #actions selected from local q
-            q_p = self.q_target(s_p).gather(1, a_p)    #actions evaluated from target q
+            q_p = torch.max(self.q_target(s_p), dim = 1).unsqueeze(1)
             y = r + self.gamma * q_p * (1 - d)
             
         loss = F.mse_loss(q, y)
@@ -71,32 +69,26 @@ class DDQN:
     def hard_update(self):
         self.q_target = copy.deepcopy(self.q_func)
 
-    def soft_update(self):
-        for target_param, param in zip(self.q_target.parameters(), self.q_func.parameters()):
-                target_param.data.copy_(param.data * self.tau + target_param.data * (1.0 - self.tau))
-
     def select_action(self, s):
         self.EPS = max(self.EPS_END, self.EPS * self.EPS_DECAY)
         if torch.rand(1) > self.EPS:
-            a = torch.argmax(self.q_func(torch.from_numpy(s).type(torch.float32)).detach()).numpy()
+            a = torch.argmax(self.q_func(torch.from_numpy(s).float()).detach()).numpy()
         else:
             a = self.environment.action_space.sample()
 
         return a
 
 def main():
-    #env_name = "CartPole-v0"
-    env_name = "LunarLander-v2"
+    env_name = "CartPole-v0"
+    # env_name = "LunarLander-v2"
     env = gym.make(env_name)
     env = RecordEpisodeStatistics(env)
 
     obs_dim = env.observation_space.shape[0]
     act_dim = env.action_space.n    #shape[0]
 
-    net = Q_duelling(obs_dim, act_dim)
-    dqn_agent = DDQN(env, net, train_after=250, target_update=300, batch_size=32, verbose=2000, learning_rate=0.004)    #hyperparameters for lunarlander
-
-    replay_buffer = ReplayBuffer(buffer_len=100000)
+    net = Q_val(obs_dim, act_dim)
+    dqn_agent = DQN(env, net, train_after=250, target_update=300, batch_size=32, verbose=2000, learning_rate=0.004)    #hyperparameters for lunarlander
 
     episodes = 0
     s_t = env.reset()
@@ -112,8 +104,8 @@ def main():
         if len(replay_buffer) >= dqn_agent.batch_size and i >= dqn_agent.train_after:
             
             if i % dqn_agent.train_freq == 0:
-                batch = replay_buffer.sample(dqn_agent.batch_size)
-                loss = dqn_agent.update(batch)
+                # batch = Transition(*zip(*random.sample(replay_buffer, k=dqn_agent.batch_size)))
+                # loss = dqn_agent.update(batch)
 
             if i % dqn_agent.target_update == 0:
                 dqn_agent.hard_update()
@@ -138,9 +130,7 @@ def main():
         s_t = s_tp1 
         if done:
             print(f'Episode Complete, reward = {info["episode"]["r"]}')
-            s_t = env.reset()
-
-           
+            s_t = env.reset()           
 
 if __name__ == "__main__":
     # stuff only to run when not called via 'import' here
