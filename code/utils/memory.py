6f4d292d1fc39e9be4257348b9cd81dc5e09cf00
mmcaulif <mmcaulif@tcd.ie>
2022-11-25 20:36:21 +0000

    added hidden dims to rollout buffer

--- code/utils/memory.py
+++ code/utils/memory.py
@@ -28,31 +28,35 @@ class ReplayBuffer():
 
 class Rollout_Memory(object):
     def __init__(self):
-        self.states, self.actions, self.rewards, self.dones, self.policies = [], [], [], [], []
-        self.v_hiddens, self.pi_hiddens = [], []
+        self.states, self.actions, self.rewards, self.dones, self.policies, self.next_states = [], [], [], [], [], []
+        self.v_hiddens, self.v_hnexts, self.pi_hiddens = [], [], []
     
-    def push(self, state, action, reward, done, policy, v_hidden, pi_hidden):
+    def push(self, state, action, reward, next_state, done, policy, v_hidden, v_hnext, pi_hidden):
         self.states.append(state)
         self.actions.append(action)
         self.rewards.append(reward)
+        self.next_states.append(next_state)
         self.dones.append(done)
         self.policies.append(policy)
         self.v_hiddens.append(v_hidden)
+        self.v_hnexts.append(v_hnext)
         self.pi_hiddens.append(pi_hidden)
 
     def pop_all(self):
         states = torch.from_numpy(np.array(self.states)).float()
         actions = torch.from_numpy(np.array(self.actions)).float()
         rewards = torch.FloatTensor(self.rewards)
+        next_states = torch.from_numpy(np.array(self.next_states)).float()
         dones = torch.IntTensor(self.dones)
         policies = torch.FloatTensor(self.policies)
         v_hiddens = torch.stack(self.v_hiddens)
+        v_hnexts = torch.stack(self.v_hnexts)
         pi_hiddens = torch.stack(self.pi_hiddens)
         
-        self.states, self.actions, self.rewards, self.dones, self.policies = [], [], [], [], []
-        self.v_hiddens, self.pi_hiddens = [], []
+        self.states, self.actions, self.rewards, self.dones, self.policies, self.next_states = [], [], [], [], [], []
+        self.v_hiddens, self.v_hnexts, self.pi_hiddens = [], [], []
         
-        return states, actions, rewards, dones, policies, v_hiddens, pi_hiddens
+        return states, actions, rewards, next_states, dones, policies, v_hiddens, v_hnexts, pi_hiddens
 
     def __len__(self):
         return len(self.states)
